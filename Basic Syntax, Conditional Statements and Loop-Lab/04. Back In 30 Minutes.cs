566af9e435e7e75d09eded24a6544ae1fc68901c
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-03-15 03:38:54 +0200

    Create 04. Back In 30 Minutes.cs

---
+++ Basic Syntax, Conditional Statements and Loop-Lab/04. Back In 30 Minutes.cs
@@ -0,0 +1,27 @@
+using System;
+
+
+namespace TimeCalculation
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int hour = int.Parse(Console.ReadLine());
+                int minutes = int.Parse(Console.ReadLine());
+
+                minutes += 30;
+                if (minutes >= 60)
+                {
+                    hour++;
+                    minutes -= 60;
+                }
+
+                if (hour > 23)
+                {
+                    hour = 0;
+                }
+                Console.WriteLine($"{hour}:{minutes:d2}");
+            }
+        }
+    }

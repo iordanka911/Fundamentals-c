6fadc462d4c494cbb9f2c959b0e72990bc914dde
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-03-16 15:11:48 +0200

    Create 12. Even Number.cs

---
+++ Basic Syntax, Conditional Statements and Loop-Lab/12. Even Number.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            while (number%2!=0)
+            {
+                Console.WriteLine("Please write an even number.");
+                number = int.Parse(Console.ReadLine());
+            }
+             Console.WriteLine($"The number is: {Math.Abs(number)}");
+            
+        }
+    }
+}

11ea2eb8db564a256feae035b2e12ba7e69acb8d
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-03-26 11:15:38 +0200

    Create 2.Common Elements.cs

---
+++ Arrays - Exercise/2.Common Elements.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = Console.ReadLine().Split(" ");
+            string[] arr2 = Console.ReadLine().Split(" ");
+
+            foreach (var elementTwo in arr2)
+            {
+                for (int i = 0; i < arr1.Length; i++)
+
+                {
+                    if (elementTwo == arr1[i])
+                    {
+                        Console.Write(elementTwo + " ");
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}

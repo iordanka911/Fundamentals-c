b07d19138fcc06c180bb691a92ddf29ee4dd7650
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-03-29 14:20:45 +0300

    Create HelloWorldMethod.cs

---
+++ Methods/HelloWorldMethod.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print("Hello world!");
+        }
+        static void Print(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}

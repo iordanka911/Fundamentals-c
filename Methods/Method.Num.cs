ae936d463c507b712018a0dd628275c8626b0ab0
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-03-29 14:24:21 +0300

    Create Method.Num.cs

---
+++ Methods/Method.Num.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print();
+        }
+        static void Print()
+        {
+            int number = 5;
+            Console.WriteLine(number);
+        }
+    }
+}

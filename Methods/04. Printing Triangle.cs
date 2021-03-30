ea41de677c77d26d82257d01be0eeb3aa1836c99
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-03-30 16:35:48 +0300

    Create 04. Printing Triangle.cs

---
+++ Methods/04. Printing Triangle.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Linq;
+
+
+namespace ConsoleApp72
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintTriangle(n);
+        }
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <=end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        static void PrintTriangle(int n)
+        {
+            for (int line = 1; line <=n; line++)
+            {
+                PrintLine(1, line);
+            }
+
+            for (int line = n-1; line >=1; line--)
+            {
+                PrintLine(1, line);
+            }
+        }
+    }
+}

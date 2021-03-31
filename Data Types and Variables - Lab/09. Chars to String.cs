861be02f41ab9c54549e666ca9a3ac26538435b5
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-03-31 18:49:21 +0300

    Create 09. Chars to String.cs

---
+++ Data Types and Variables - Lab/09. Chars to String.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace TownInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            char thirdChar = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstChar}{secondChar}{thirdChar}");
+
+        }
+    }
+}

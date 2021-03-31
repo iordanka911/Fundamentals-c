9c35304580816922575b2d18229c70b3021de9ea
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-03-31 18:21:25 +0300

    Create 06. Reversed Chars.cs

---
+++ Data Types and Variables - Lab/06. Reversed Chars.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Reverse_chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            char thirdChar = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{thirdChar} {secondChar} {firstChar}");
+
+        }
+    }
+}

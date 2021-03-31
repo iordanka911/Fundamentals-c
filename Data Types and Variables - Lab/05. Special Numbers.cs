eb422e4c91cb5295b3b3b6435777b55ac487533f
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-03-31 15:57:37 +0300

    Create 05. Special Numbers.cs

---
+++ Data Types and Variables - Lab/05. Special Numbers.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+using System.Numerics;
+
+
+namespace ExactSumoRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int num = 1; num <= n; num++)
+
+            {
+
+                int sumOfDigits = 0;
+
+                int digits = num;
+
+                while (digits > 0)
+
+                {
+
+                    sumOfDigits += digits % 10;
+
+                    digits = digits / 10;
+
+                }
+                if (sumOfDigits==5||sumOfDigits==7||sumOfDigits==11)
+                {
+                    Console.WriteLine($"{num} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{num} -> False");
+                }
+
+                
+
+            }
+
+
+        }
+
+
+    }
+}

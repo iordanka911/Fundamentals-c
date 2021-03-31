3fc9628bec743b348dea7d29f40a6af45bdaa145
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-01 01:13:31 +0300

    Create 02. English Name of the Last Digit.cs

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/02. English Name of the Last Digit.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+
+namespace sortNumber
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int lastDigit = number % 10;
+            if (lastDigit==0)
+            {
+                Console.WriteLine("zero");
+            }
+            if (lastDigit == 1)
+            {
+                Console.WriteLine("one");
+            }
+            if (lastDigit == 2)
+            {
+                Console.WriteLine("two");
+            }
+            if (lastDigit == 3)
+            {
+                Console.WriteLine("three");
+            }
+            if (lastDigit == 4)
+            {
+                Console.WriteLine("four");
+            }
+            if (lastDigit == 5)
+            {
+                Console.WriteLine("five");
+            }
+            if (lastDigit == 6)
+            {
+                Console.WriteLine("six");
+            }
+            if (lastDigit == 7)
+            {
+                Console.WriteLine("seven");
+            }
+            if (lastDigit == 8)
+            {
+                Console.WriteLine("eight");
+            }
+            if (lastDigit == 9)
+            {
+                Console.WriteLine("nine");
+            }
+        }
+    }
+}

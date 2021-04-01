0d1b60df1564d6222a665a81200a016367666400
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-02 00:40:54 +0300

    Create Numbers in Reversed order 2 .cs

---
+++ Lab: Arrays/Numbers in Reversed order 2 .cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = n-1; i >=0; i--)
+            {
+                Console.Write("{0} ",arr[i]);
+            }
+        }
+    }
+}

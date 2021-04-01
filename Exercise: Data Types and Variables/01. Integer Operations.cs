e8d8fa84be2119228dbd451e417fc722a65056b3
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-01 20:47:52 +0300

    Create 01. Integer Operations.cs

---
+++ Exercise: Data Types and Variables/01. Integer Operations.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace IntegerOperations
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            int result = ((firstNumber + secondNumber) / thirdNumber) * fourthNumber;
+            Console.WriteLine(result);
+        }
+    }
+}

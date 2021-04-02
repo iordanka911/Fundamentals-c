3c61c647627af6deeb8e109d2bcf81f617c8b007
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-02 23:41:05 +0300

    Update Problem 2. Mu Online.cs

--- 05. Programming Fundamentals Mid Exam/Problem 2. Mu Online.cs
+++ 05. Programming Fundamentals Mid Exam/Problem 2. Mu Online.cs
@@ -1,57 +1,56 @@
 using System;
 
-namespace _2_Zadacha_Izpit
+namespace MuOnline
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] staii = Console.ReadLine().Split('|');
+            string[] dungeonsRooms = Console.ReadLine().Split('|');
 
-            int zdrave = 100;
-            int bitkoins = 0;
+            int health = 100;
+            int bitcoins = 0;
             int count = 0;
-            for (int i = 0; i < staii.Length - 1; i++)
+            for (int i = 0; i < dungeonsRooms.Length - 1; i++)
             {
-                int curenthell = 0;
                 count++;
-                string[] splited = staii[i].Split();
+                string[] splited = dungeonsRooms[i].Split();
                 string split0 = splited[0];
 
                 string split1 = splited[1];
                 if (split0 == "potion")
                 {
-                    if (zdrave < 100)
+                    if (health < 100)
                     {
-                        zdrave += int.Parse(split1);//
+                        health += int.Parse(split1);//
                         int splait = int.Parse(split1);
 
-                        if (zdrave > 100)
+                        if (health > 100)
                         {
-                            splait = 100 - zdrave;
-                            zdrave = 100;
+                            splait = 100 - health;
+                            health = 100;
                         }
                         Console.WriteLine($"You healed for {splait} hp.");
-                        Console.WriteLine($"Current health: {zdrave} hp.");
+                        Console.WriteLine($"Current health: {health} hp.");
                     }
                 }
                 else if (split0 == "chest")
                 {
-                    bitkoins += int.Parse(split1);
+                    bitcoins += int.Parse(split1);
                     Console.WriteLine($"You found {int.Parse(split1)} bitcoins.");
                 }
                 else
                 {
-                    int silanaChudovishteto = int.Parse(split1);
+                    int powerOfMonster = int.Parse(split1);
 
-                    if (silanaChudovishteto >= 0 && silanaChudovishteto <= 100)
+                    if (powerOfMonster >= 0 && powerOfMonster <= 100)
                     {
-                        zdrave -= silanaChudovishteto;
-                        if (zdrave > 0)
+                        health -= powerOfMonster;
+                        if (health > 0)
                         {
                             Console.WriteLine($"You slayed {split0}.");
                         }
-                        else if (zdrave <= 0)
+                        else if (health <= 0)
                         {
                             Console.WriteLine($"You died! Killed by {split0}.");
                             Console.WriteLine($"Best room: {count}");
@@ -62,11 +61,11 @@ namespace _2_Zadacha_Izpit
                 }
 
             }
-            if (count == staii.Length)//-1
+            if (count == dungeonsRooms.Length)
             {
                 Console.WriteLine("You've made it!");
-                Console.WriteLine($"Bitcoins: {bitkoins}");
-                Console.WriteLine($"Health: {zdrave}");
+                Console.WriteLine($"Bitcoins: {bitcoins}");
+                Console.WriteLine($"Health: {health}");
             }
         }
     }

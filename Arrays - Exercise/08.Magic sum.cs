02be325b121ac9c3a181c348cb2e69d61c35614e
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-04 19:27:18 +0300

    Create 08.Magic sum.cs

---
+++ Arrays - Exercise/08.Magic sum.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+
+namespace Magic sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                             .Split(" ")
+                             .Select(int.Parse)
+                             .ToArray();
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int firstNumber = arr[i];
+
+                for (int j = i+1; j <arr.Length; j++)
+                {
+                    int secondNumber = arr[j];
+
+                    if (firstNumber+secondNumber==number)
+                    {
+                        Console.WriteLine($"{firstNumber} {secondNumber}");
+                    }
+                }
+            }
+         }
+    }
+}

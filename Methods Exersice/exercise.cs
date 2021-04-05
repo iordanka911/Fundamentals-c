b284eb839ce5ff49e242d197785a6e89d0ab0137
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-05 16:31:34 +0300

    Create exercise.cs

---
+++ Methods Exersice/exercise.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello world!";
+
+            PrintText(text);
+        }
+        public static void PrintText(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+    }
+   
+}

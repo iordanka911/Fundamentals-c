14a4a18d944bedb29821c2b4828e29537707d8df
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-06 10:22:09 +0300

    Create DecToHex.cs

---
+++ Bitwise Operations/DecToHex.cs
@@ -0,0 +1,58 @@
+using System;
+
+namespace ConvertDecimalToBinarySystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM = 500;
+            string result = "";
+            while (NUM>0)
+            {
+                int d = NUM % 16;
+                result = GetHexDigit(d) + result;
+                NUM = NUM / 16;
+
+            }
+            Console.WriteLine(result);
+        }
+
+        private static string GetHexDigit(int d)
+        {
+            if (d<=10)
+            {
+                return "" + d;
+            }
+            else if (d==10)
+            {
+                return "A";
+            }
+            else if (d == 11)
+            {
+                return "B";
+            }
+            else if (d == 12)
+            {
+                return "C";
+            }
+            else if (d == 13)
+            {
+                return "D";
+            }
+            else if (d == 14)
+            {
+                return "E";
+            }
+            else if (d == 15)
+            {
+                return "F";
+            }
+            else
+            {
+                throw new Exception("Invalid digit");
+            }
+        }
+    }
+   
+}

8dfb949fb567099ae57c0ff996ade903ad5fb52d
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-07 17:37:18 +0300

    Create 05. Remove Negatives and Reverse.cs

---
+++ Lists/05. Remove Negatives and Reverse.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                     .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                     .Select(int.Parse)
+                     .ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i]<0)
+                {
+                    numbers.RemoveAt(i--);
+                }
+            }
+
+            numbers.Reverse();
+            if (numbers.Count==0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+
+        }
+    }
+}

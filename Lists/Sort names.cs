99c64829b8184f27ce4c98a82dd32621224f6de5
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-07 17:19:10 +0300

    Create Sort names.cs

---
+++ Lists/Sort names.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>()
+          {"Peter", "Michael", "George", "Victor", "John"};
+            names.Sort();
+            Console.WriteLine(string.Join(", ",names));
+             
+        }
+    }
+}

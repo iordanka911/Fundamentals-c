0a64dc7b41c373e4c08a029569cbaa09c9ad950c
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-22 21:56:48 +0300

    Update targets.cs

--- 05. Programming Fundamentals Mid Exam/targets.cs
+++ 05. Programming Fundamentals Mid Exam/targets.cs
@@ -1,50 +1,90 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace MovingTarget
+namespace ConsoleApp48
 {
     class Program
     {
         static void Main(string[] args)
         {
             List<int> targets = Console.ReadLine()
-                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                 .Select(int.Parse)
-                 .ToList();
+                             .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                             .Select(int.Parse)
+                             .ToList();
 
             string input;
 
             while ((input=Console.ReadLine()?.ToUpper())!="END")
             {
-                string[] tokens = input
-                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                string command = tokens[0];
+                string[] tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0]?.ToUpper();
                 int index = int.Parse(tokens[1]);
                 int value = int.Parse(tokens[2]);
 
-                if (index<0||index>=targets.Count)
-                {
-                    continue;
-                }
+              
                 switch (command)
                 {
                     case "SHOOT":
-                        targets[index] -= value;
+                        if (index < 0 || index >= targets.Count)
+                        {
+                            
+                            continue;
+                        }
+                        else
+                        {
+                            targets[index] -= value;
+                            if (targets[index] <= 0)
+                            {
+                                targets.RemoveAt(index);
+                            }
+                        }
+                        
+
                         break;
                     case "ADD":
-
+                        if (index < 0 || index >= targets.Count)
+                        {
+                            
+                         Console.WriteLine("Invalid placement!");
+                          
+                        }
+                        else
+                        {
+                            targets.Insert(index, value);
+                        }
+                        
                         break;
                     case "STRIKE":
+                        if (index < 0 || index >= targets.Count)
+                        {
+                           
+                           Console.WriteLine("Strike missed!");
+                          
+                            continue;
+                        }
+                        else
+                        {
+                            if (index - value < 0 || index + value >= targets.Count)
+                            {
+                                Console.WriteLine("Strike missed!");
 
+                                continue;
+                            }
+
+
+                            targets.RemoveRange(index - value, (value * 2) + 1);
+                            
+                        }
+                       
                         break;
 
-
-
-                    default:
+                    default: 
                         break;
                 }
             }
+
+            Console.WriteLine(string.Join('|',targets));
         }
     }
 }

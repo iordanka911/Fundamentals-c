e175a75ad6caa5e8adaa929d1eeb3bb96f6c35dd
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-22 09:18:21 +0300

    Create 03. House Party.cs

---
+++ List exercises/03. House Party.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HouseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> guests = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArg = Console.ReadLine().Split();
+                string guestName = cmdArg[0];
+
+                if (cmdArg.Length>3)
+                {
+                    if (guests.Contains(guestName))
+                    {
+                        guests.Remove(guestName);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{guestName} is not in the list!");
+                    }
+                }
+                else
+                {
+                    if (!guests.Contains(guestName))
+                    {
+                        guests.Add(guestName);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{guestName} is already in the list!");
+                    }
+                }
+
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, guests));
+        }
+    }
+}

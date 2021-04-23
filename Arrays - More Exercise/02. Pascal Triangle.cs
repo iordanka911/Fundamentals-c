8b8a16876465e055c16dd6575ba2b078c04ba87a
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-23 22:29:17 +0300

    Create 02. Pascal Triangle.cs

---
+++ Arrays - More Exercise/02. Pascal Triangle.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ConsoleApp49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dimension = int.Parse(Console.ReadLine());
+            Console.WriteLine(1);
+
+            int[] previous = new int[] { 1 };
+
+            for (int i = 2; i <= dimension; i++)
+            {
+                int[] values = new int[i];
+                values[0] = 1;
+                values[i - 1] = 1;
+
+                for (int j = 1; j < i-1; j++)
+                {
+                    values[j] = previous[j - 1] + previous[j];
+                }
+                Console.WriteLine(string.Join(' ',values));
+                previous = values;
+            }
+        }
+    }
+}

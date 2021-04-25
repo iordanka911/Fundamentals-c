fd80b4fc526680b209ef4caf08e632b89d67dc65
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-04-25 09:37:56 +0300

    Create 03. Word Synonyms.cs

---
+++ Assosiative Arrays/03. Word Synonyms.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumberOfWords = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+            for (int i = 0; i < NumberOfWords; i++)
+            {
+                string word = Console.ReadLine();
+                string synonim = Console.ReadLine();
+
+                if (dictionary.ContainsKey(word)) 
+                {
+                    dictionary[word].Add(synonim);
+                }
+                else
+                {
+                    dictionary.Add(word, new List<string>() { synonim });
+                }
+            }
+            foreach (var word in dictionary)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ",word.Value)}");
+            }
+
+        }
+    }
+}

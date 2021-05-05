2f706550cbbe296d466c669f248176fd34b8c80d
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-05-05 11:47:21 +0300

    Create 02. From Left to The Right.cs

---
+++ Data Types and Variables - More Exercise/02. From Left to The Right.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace _02._From_Left_to_The_Right
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string numbers = Console.ReadLine();
+                char delimiter = ' ';
+                string[] splitNumbers = numbers.Split(delimiter);
+
+                long leftNum = long.Parse(splitNumbers[0]);
+                long rightNum = long.Parse(splitNumbers[1]);
+
+                long biggerNumber = rightNum;
+                if (leftNum > rightNum)
+                {
+                    biggerNumber = leftNum;
+                }
+
+                long sumOfDigits = 0;
+                while (biggerNumber != 0)
+                {
+                    sumOfDigits += biggerNumber % 10;
+                    biggerNumber /= 10;
+                }
+                Console.WriteLine(Math.Abs(sumOfDigits));
+            }
+        }
+    }
+}

5037a83980efccdab4206eb0b1f37117db136b94
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-05-09 23:22:43 +0300

    Create 02. A Miner Task.cs

---
+++ Associative Arrays - Exercise/02. A Miner Task.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace MinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> output = new Dictionary<string, int>();
+
+            string resource = Console.ReadLine();
+
+            while (resource!="stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!output.ContainsKey(resource))
+                {
+                    output.Add(resource, 0);
+                }
+                output[resource] += quantity;
+                resource = Console.ReadLine();
+            }
+            foreach (var item in output)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}

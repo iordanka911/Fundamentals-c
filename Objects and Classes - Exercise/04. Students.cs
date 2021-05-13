b54bc98f79938017cfd247df9d13270bde0fd5eb
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-05-13 16:25:22 +0300

    Create 04. Students.cs

---
+++ Objects and Classes - Exercise/04. Students.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Linq;
+
+namespace articles1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                string firstName=tokens[0];
+                string lastName = tokens[1];
+                double grade = double.Parse(tokens[2]);
+
+                Student student = new Student(firstName,lastName,grade);
+
+                students.Add(student);
+            }
+
+            students = students.OrderByDescending(x => x.Grade).ToList();
+
+            foreach (Student currentStudent in students)
+            {
+                Console.WriteLine(currentStudent);
+            }
+        }
+    }  
+    class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:f2}";
+        }
+    }
+}

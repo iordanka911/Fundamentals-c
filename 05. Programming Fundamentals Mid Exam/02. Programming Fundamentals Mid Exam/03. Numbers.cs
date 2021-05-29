8d5e6c62b730e43aa5532e3f8bf791260db5c8b6
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-05-29 16:45:57 +0300

    Create 03. Numbers.cs

---
+++ 05. Programming Fundamentals Mid Exam/02. Programming Fundamentals Mid Exam/03. Numbers.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            double avg = numbers.Average();
+            int[] result = numbers
+                .Where(x => x > avg)
+                .OrderByDescending(n => n)
+                .Take(5)
+                .ToArray();
+
+            if (result.Length==0)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", result));
+            }
+        }
+    }
+}

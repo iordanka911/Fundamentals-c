416b552faa50c6f42957d317351bcca2b7ceaeb0
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-06-05 00:15:29 +0300

    Update 10. Poke Mon.cs

--- Exercise: Data Types and Variables/10. Poke Mon.cs
+++ Exercise: Data Types and Variables/10. Poke Mon.cs
@@ -28,3 +28,38 @@ namespace beerKeg
         }
     }
 }
+
+
+
+using System;
+using System.Linq;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            int halfOfN = n / 2;
+
+            int target = 0;
+
+            while(n>=m)
+            {
+                target++;
+                n -= m;
+                if (halfOfN==n&&y>0)
+                {
+                    n /= y;
+                }
+                
+            }
+            Console.WriteLine(n);
+            Console.WriteLine(target);
+        }
+    }
+}

5742d1ebf84a9410b8199e7fdeb016bd155cc217
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-06-04 22:29:13 +0300

    Update 05. Print Part Of ASCII Table.cs

--- Exercise: Data Types and Variables/05. Print Part Of ASCII Table.cs
+++ Exercise: Data Types and Variables/05. Print Part Of ASCII Table.cs
@@ -13,3 +13,25 @@ class Program
         }
     }
 }
+
+
+using System;
+using System.Linq;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (char i = (char)start; i <=(char)end; i++)
+            {
+                Console.Write(i+" ");
+            }
+           
+        }
+    }
+}

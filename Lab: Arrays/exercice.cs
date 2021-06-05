d348e3c76ef2cd616c1edc3083dae3c30c0eb1d4
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-06-05 08:29:54 +0300

    Update exercice.cs

--- Lab: Arrays/exercice.cs
+++ Lab: Arrays/exercice.cs
@@ -17,3 +17,24 @@ namespace ConsoleApp75
         }
     }
 }
+
+
+using System;
+
+namespace ConsoleApp75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i*5;
+
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}

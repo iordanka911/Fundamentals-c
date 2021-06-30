02c8bb17e6e0564888f51724935b8a0d4124a148
iordanka911 <78705669+iordanka911@users.noreply.github.com>
2021-07-01 01:07:16 +0300

    Create Arrows.cs

---
+++ 05. Programming Fundamentals Mid Exam/Arrows.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Arrows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] targets = Console.ReadLine().Split('|').Select(int.Parse).ToArray();
+            int points = 0;
+            string input = Console.ReadLine();
+            while (input!="Game over")
+            {
+                string[] splitted = input.Split();
+                if (splitted[0]=="Reverse")
+                {
+                   targets= targets.Reverse().ToArray();
+                }
+                if (splitted[0]=="Shoot")
+                {
+                    string[] shooting = splitted[1].Split('@');
+
+                    if (shooting[0]=="Left")
+                    {
+                        int index = int.Parse(shooting[1]);
+                        int count = int.Parse(shooting[2]);
+                        if (index>=0&&index<targets.Length)
+                        {
+                            for (int i = 0; i < count; i++)
+                            {
+                                index--;
+                                if (index==-1)
+                                {
+                                    index = targets.Length - 1;
+                                }
+                            }
+
+                            if (targets[index]>=5)
+                            {
+                                targets[index] -= 5;
+                                points += 5;
+                            }
+                            else if (targets[index] > 0)
+                            {
+                                targets[index] = 0;
+                                points += targets[index];
+                            }
+
+                        }
+                    }
+
+
+                    if (shooting[0] == "Right")
+                    {
+                        int index = int.Parse(shooting[1]);
+                        int count = int.Parse(shooting[2]);
+                        if (index >= 0 && index < targets.Length)
+                        {
+                            for (int i = 0; i < count; i++)
+                            {
+                                index++;
+                                if (index == targets.Length)
+                                {
+                                    index = 0;
+                                }
+                            }
+
+                            if (targets[index] >= 5)
+                            {
+                                targets[index] -= 5;
+                                points += 5;
+                            }
+                            else if(targets[index]>0)
+                            {
+                                targets[index] = 0;
+                                points += targets[index];
+                            }
+
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" - ", targets));
+            Console.WriteLine($"Iskren finished the archery tournament with {points}!");
+        }
+    }
+}
